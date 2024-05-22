7c5a088ce928bcb6ee9d59699d2e6c14b8139e80
Surya_Jas <89996645+Sur-Jas@users.noreply.github.com>
2024-05-22 17:15:53 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -66,6 +66,12 @@ st.write(
     """
     -------Hola ! Enjoy your beverage-------
 """)
+
+
+
+import requests
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+st.text(fruityvice_response)
     
 
 
