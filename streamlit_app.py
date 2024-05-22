beff1db08e06c0a38b980ba459b8be670b18461d
Surya_Jas <89996645+Sur-Jas@users.noreply.github.com>
2024-05-22 11:42:52 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -69,6 +69,7 @@ st.write(
 
 import requests
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-st.text(fruityvice_response)
+st.text(fruityvice_response.json())
+fv_df = st.dataframe(data = fruityvice_response.json(), use_container_width=True)
     
 
